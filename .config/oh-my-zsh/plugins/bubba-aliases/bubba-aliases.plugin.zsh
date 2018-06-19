f77dfa19e410872ce822d77704b9f84ed51fa55a
Bubba <rob@stechstudio.com>
2018-06-19 17:26:36 +0000

    My Plugins

---
+++ .config/oh-my-zsh/plugins/bubba-aliases/bubba-aliases.plugin.zsh
@@ -0,0 +1,53 @@
+# because typing 'cd' is A LOT of work!!
+alias ..='cd ../'
+alias ...='cd ../../'
+alias ....='cd ../../../'
+alias .....='cd ../../../../'
+
+# ls, the common ones I use a lot shortened for rapid fire usage
+alias ls="$BIN_LS --color" # use gnu ls, and color
+alias l='ls -lFh'     #size,show type,human readable
+alias lb='ls -lAFh'   #long list,show almost all,show type,human readable
+alias lr='ls -tRFh'   #sorted by date,recursive,show type,human readable
+alias lt='ls -ltFh'   #long list,sorted by date,show type,human readable
+alias ll='ls -l'      #long list
+
+alias zshrc='$EDITOR ~/.zshrc' # Quick access to the ~/.zshrc file
+alias resetZsh='source ~/.zshrc'
+
+alias clr='clear'
+alias cls='clear'
+
+alias grep='grep --color'
+alias sgrep='grep -R -n -H -C 5 --exclude-dir={.git,.svn,CVS} '
+alias hgrep='alias hgrep="fc -El 0 | grep'
+
+alias t='tail -f'
+
+alias vi='nvim'
+alias :w='echo "Dude.. $SHELL != vi"'
+alias :q='echo "Dude.. $SHELL != vi"'
+alias :wq='echo "Dude.. $SHELL != vi"'
+alias ssht='/usr/bin/ssh -f -N'
+
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+
+#Composer Aliases
+alias c='composer'
+alias csu='composer self-update'
+alias cu='composer update'
+alias ci='composer install'
+alias ccp='composer create-project'
+alias cdu='composer dump-autoload'
+
+alias ap='ansible-playbook'
+
+
+alias cat='colorize_via_pygmentize'
+
+alias bubba="Hello Studly"
+
+# Make zsh know about hosts already accessed by SSH
+zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
---
+++ .config/oh-my-zsh/plugins/bubba-exports/bubba-exports.plugin.zsh
@@ -0,0 +1,46 @@
+export PATH=/bin
+export PATH=/sbin:$PATH
+export PATH=/usr/bin:$PATH
+export PATH=/usr/sbin:$PATH
+export PATH=/usr/local/bin:$PATH
+export PATH=/usr/local/sbin:$PATH
+export PATH=/Users/bubba/.composer/vendor/bin:$PATH
+export PATH=$HOME/bin:$PATH
+export PATH=.:$PATH
+
+
+
+if [[ `/usr/bin/uname` == 'Linux' ]]; then
+     export LINUX=1
+    export BIN_DIRCOLORS=`which dircolors`
+    export BIN_LS=/usr/bin/ls
+else
+    export LINUX=0
+fi
+
+if [[ `/usr/bin/uname` == 'Darwin' ]]; then
+    export OSX=1
+    export BIN_DIRCOLORS=`which gdircolors`
+    export BIN_LS=/usr/local/bin/gls
+else
+    export OSX=0
+fi
+
+
+
+export HOSTNAME="`hostname -s`"
+export PAGER="less"
+export EDITOR="nvim"
+
+#GIT Settings
+export GIT_AUTHOR_NAME=Bubba
+export GIT_AUTHOR_EMAIL=rob@stechstudio.com
+export GIT_COMMITTER_NAME=Bubba
+export GIT_COMMITTER_EMAIL=rob@stechstudio.com
+
+export STS_USER=bubba
+
+# Export Colors for LS
+if [[ -a $ZSH_CUSTOM/dircolors-solarized/dircolors.256dark ]]; then
+    eval `$BIN_DIRCOLORS $ZSH_CUSTOM/dircolors-solarized/dircolors.256dark`
+fi
