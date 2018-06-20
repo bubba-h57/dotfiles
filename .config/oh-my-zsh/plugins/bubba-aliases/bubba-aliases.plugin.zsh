b753dc7db4bca43b6c2818a2ba9bb9109b392004
Bubba <rob@stechstudio.com>
2018-06-20 15:39:12 +0000

    Moar Aliases

--- .config/oh-my-zsh/plugins/bubba-aliases/bubba-aliases.plugin.zsh
+++ .config/oh-my-zsh/plugins/bubba-aliases/bubba-aliases.plugin.zsh
@@ -42,12 +42,13 @@ alias ci='composer install'
 alias ccp='composer create-project'
 alias cdu='composer dump-autoload'
 
-alias ap='ansible-playbook'
-
+alias ap='ansible-playbook --private-key ansible.pem --vault-password-file .ansible-vault'
 
 alias cat='colorize_via_pygmentize'
 
 alias bubba="Hello Studly"
+
+alias projects="cd $HOME/Development"
 
 # Make zsh know about hosts already accessed by SSH
 zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
