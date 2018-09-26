f475f9da472b25efd1900d70be967441f02c94d5
Bubba <rob@stechstudio.com>
2018-09-26 12:58:34 -0400

    Alias me

--- .config/oh-my-zsh/plugins/bubba-aliases/bubba-aliases.plugin.zsh
+++ .config/oh-my-zsh/plugins/bubba-aliases/bubba-aliases.plugin.zsh
@@ -52,3 +52,4 @@ alias projects="cd $HOME/Development"
 
 # Make zsh know about hosts already accessed by SSH
 zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
+alias qami="{ whoami; echo '@'; echo $HOSTNAME; echo ' in '; pwd;}| sed ':a;N;s/\n//;ba'"
