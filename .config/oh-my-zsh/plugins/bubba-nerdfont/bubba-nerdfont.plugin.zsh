ce02d31acb4a82054f1b8e6d22da66fc60797aa3
Bubba <rob@stechstudio.com>
2019-07-10 15:25:08 -0400

    Nerdfont plugin'

---
+++ .config/oh-my-zsh/plugins/bubba-nerdfont/bubba-nerdfont.plugin.zsh
@@ -0,0 +1,66 @@
+cd ~/Library/Fonts
+
+fontname="Sauce Code Pro Black Italic Nerd Font Complete.ttf"
+if [ ! -f ~/Library/Fonts/$fontname ]; then
+    curl -fLo $fontname https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/SourceCodePro/Black-Italic/complete/Sauce%20Code%20Pro%20Black%20Italic%20Nerd%20Font%20Complete.ttf
+fi
+
+fontname="Sauce Code Pro Black Nerd Font Complete.ttf"
+if [ ! -f ~/Library/Fonts/$fontname ]; then
+    curl -fLo $fontname https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/SourceCodePro/Black/complete/Sauce%20Code%20Pro%20Black%20Nerd%20Font%20Complete.ttf
+fi
+ 
+fontname="Sauce Code Pro Bold Italic Nerd Font Complete.ttf"
+if [ ! -f ~/Library/Fonts/$fontname ]; then
+    curl -fLo $fontname https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/SourceCodePro/Bold-Italic/complete/Sauce%20Code%20Pro%20Bold%20Italic%20Nerd%20Font%20Complete.ttf
+fi
+
+fontname="Sauce Code Pro Bold Nerd Font Complete.ttf"
+if [ ! -f ~/Library/Fonts/$fontname ]; then
+    curl -fLo $fontname https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/SourceCodePro/Bold/complete/Sauce%20Code%20Pro%20Bold%20Nerd%20Font%20Complete.ttf
+fi
+
+fontname="Sauce Code Pro ExtraLight Nerd Font Complete.ttf"
+if [ ! -f ~/Library/Fonts/$fontname ]; then
+    curl -fLo $fontname https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/SourceCodePro/Extra-Light/complete/Sauce%20Code%20Pro%20ExtraLight%20Nerd%20Font%20Complete.ttf
+fi
+
+fontname="Sauce Code Pro ExtraLight Italic Nerd Font Complete.ttf"
+if [ ! -f ~/Library/Fonts/$fontname ]; then
+    curl -fLo $fontname https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/SourceCodePro/ExtraLight-Italic/complete/Sauce%20Code%20Pro%20ExtraLight%20Italic%20Nerd%20Font%20Complete.ttf
+fi
+ 
+fontname="Sauce Code Pro Light Italic Nerd Font Complete.ttf"
+if [ ! -f ~/Library/Fonts/$fontname ]; then
+    curl -fLo $fontname https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/SourceCodePro/Light-Italic/complete/Sauce%20Code%20Pro%20Light%20Italic%20Nerd%20Font%20Complete.ttf
+fi
+
+fontname="Sauce Code Pro Light Nerd Font Complete.ttf"
+if [ ! -f ~/Library/Fonts/$fontname ]; then
+    curl -fLo $fontname https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/SourceCodePro/Light/complete/Sauce%20Code%20Pro%20Light%20Nerd%20Font%20Complete.ttf
+fi
+
+fontname="Sauce Code Pro Medium Italic Nerd Font Complete.ttf"
+if [ ! -f ~/Library/Fonts/$fontname ]; then
+    curl -fLo $fontname https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/SourceCodePro/Medium-Italic/complete/Sauce%20Code%20Pro%20Medium%20Italic%20Nerd%20Font%20Complete.ttf
+fi
+
+fontname="Sauce Code Pro Medium Nerd Font Complete.ttf"
+if [ ! -f ~/Library/Fonts/$fontname ]; then
+    curl -fLo $fontname https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/SourceCodePro/Medium/complete/Sauce%20Code%20Pro%20Medium%20Nerd%20Font%20Complete.ttf
+fi
+ 
+fontname="Sauce Code Pro Nerd Font Complete.ttf"
+if [ ! -f ~/Library/Fonts/$fontname ]; then
+    curl -fLo $fontname https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/SourceCodePro/Regular/complete/Sauce%20Code%20Pro%20Nerd%20Font%20Complete.ttf
+fi
+
+fontname="Sauce Code Pro Semibold Italic Nerd Font Complete.ttf"
+if [ ! -f ~/Library/Fonts/$fontname ]; then
+    curl -fLo $fontname https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/SourceCodePro/Semibold-Italic/complete/Sauce%20Code%20Pro%20Semibold%20Italic%20Nerd%20Font%20Complete.ttf
+fi
+
+fontname="Sauce Code Pro Semibold Nerd Font Complete.ttf"
+if [ ! -f ~/Library/Fonts/$fontname ]; then
+    curl -fLo $fontname https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/SourceCodePro/Semibold/complete/Sauce%20Code%20Pro%20Semibold%20Nerd%20Font%20Complete.ttf
+fi
