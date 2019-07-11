ca1e0115c5e8562737aaf78af17a54e79302fab1
Bubba <rob@stechstudio.com>
2019-07-11 12:21:15 -0400

    Moar php and python

--- .config/oh-my-zsh/plugins/bubba-brew/bubba-brew.plugin.zsh
+++ .config/oh-my-zsh/plugins/bubba-brew/bubba-brew.plugin.zsh
@@ -23,6 +23,9 @@ if [ ! -f  $brew_bin ]; then
         $brew_prefix/share \
         $brew_prefix/var/homebrew/linked \
         $brew_prefix/Cellar
+    brew install libxml2 bzip2 curl zlib git hub
+
+
 fi
 
 export PATH=/Users/bubba/homebrew/bin:$PATH
@@ -49,6 +52,24 @@ if [ ! -f  $HOME/bin/phpbrew ]; then
     curl -L -o $HOME/bin/phpbrew https://github.com/phpbrew/phpbrew/raw/master/phpbrew
     chmod +x $HOME/bin/phpbrew
     phpbrew init
+    phpbrew install 7.3 +default+fpm+dbs +bz2="$(brew --prefix bzip2)" +curl="$(brew --prefix curl)" +zlib="$(brew --prefix zlib)"
+    phpbrew switch php-7.3.7
+    EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
+    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+    if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+    then
+        >&2 echo 'ERROR: Invalid composer installer signature'
+        rm composer-setup.php
+        exit 1
+    fi
+
+    php composer-setup.php --quiet --install-dir="$HOME/bin"
+    RESULT=$?
+    rm composer-setup.php
+    mv $HOME/bin/composer.phar $HOME/bin/composer
+    echo $RESULT
 fi
 
 
